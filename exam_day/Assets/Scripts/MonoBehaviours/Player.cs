62a8ed1a30ef4c1816c6cc43864dda0dc559f7ae
dioma <up201904974@fe.up.pt>
2023-02-26 03:37:37 +0000

    Heart power up and coin consumable are created

--- exam_day/Assets/Scripts/MonoBehaviours/Player.cs
+++ exam_day/Assets/Scripts/MonoBehaviours/Player.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class Player : Character
 {
-    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+            if(hitObject !=null){
+
+                            switch(hitObject.itemType)
+            {
+                case Item.ItemType.COIN:
+                    // Add coin to inventory
+                    break;
+                case Item.ItemType.HEALTH:
+                    // Add health to player
+                    AdjustHitPoints(hitObject.quantity);
+                    break;
+            }
+
+            collision.gameObject.SetActive(false);
+                
+            }
+
+        }
+    }
+
+    public void AdjustHitPoints(int amount)
+    {
+        hitPoints += amount;
+        if (hitPoints > maxHitPoints)
+        {
+            hitPoints = maxHitPoints;
+        }
+
+        print("Player hit points: " + hitPoints);
+    }
 }
